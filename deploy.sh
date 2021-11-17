9a06e17f3791aa83c112083c9061d702c6359527
Ericsson Budhilaw <ericsson.budhilaw@gmail.com>
2021-11-17 17:34:22 +0700

    fix: github action

--- deploy.sh
+++ deploy.sh
@@ -8,10 +8,6 @@ echo "Deploying application ..."
     # Update codebase
     git fetch origin main
     git pull origin main
-
-    # chmod
-    chmod -R 775 storage
-    chmod -R ugo+rw storage
 # Exit maintenance mode
 php artisan up
 
